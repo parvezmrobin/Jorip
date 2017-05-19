9139956b3ab50d60bfcfc56783b26c098562c72c
Parvez M Robin <parvezmrobin@gmail.com>
2017-05-19 19:52:54 +0600

    @yield

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+@yield('style')
 
-    <!-- Scripts -->
+<!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
@@ -21,93 +22,89 @@
     </script>
     <style >
 
- .list-inline li {
-           margin: 0px;
+        .list-inline li {
+            margin: 0px;
 
-           display: table-cell;
-           text-align: center;
-           width: 20%;
-           font-size: xx-large;
-       }
-       body {
-       padding-top: 100px;
-   }
+            display: table-cell;
+            text-align: center;
+            width: 20%;
+            font-size: xx-large;
+        }
+        body {
+            padding-top: 100px;
+        }
 
 
 
- </style>
+    </style>
 </head>
 <body>
-    <div id="app">
-        <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header navbar-left pull-left">
+<div id="app">
+    <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header navbar-left pull-left">
 
-    </div>
-    <div class="navbar-header navbar-right pull-right">
-        <ul class="nav pull-left list-inline ">
-                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-envelope"></span></a></li>
-                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-stats"></span></a></li>
-                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-folder-open"></span></a></li>
-                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-file"></span></a></li>
-                <li class="navbar-text pull-left">
-                    <a href="#" data-toggle="collapse" data-target="#clps">
-                        <span class="glyphicon glyphicon-th-list"></span>
-                    </a>
-                </li>
-            </ul>
+            </div>
+            <div class="navbar-header navbar-right pull-right">
+                <ul class="nav pull-left list-inline ">
+                    <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-envelope"></span></a></li>
+                    <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-stats"></span></a></li>
+                    <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-folder-open"></span></a></li>
+                    <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-file"></span></a></li>
+                    <li class="navbar-text pull-left">
+                        <a href="#" data-toggle="collapse" data-target="#clps">
+                            <span class="glyphicon glyphicon-th-list"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="visible-xs-block clearfix"></div>
+            <div class="collapse" id="clps">
+                <ul class="nav navbar-nav navbar-left">
 
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Payment System</a></li>
+                    <li><a href="#">Settings</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
 
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-
-    </div>
-    <div class="visible-xs-block clearfix"></div>
-    <div class="collapse" id="clps">
-      <ul class="nav navbar-nav navbar-left">
-
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Payment System</a></li>
-          <li><a href="#">Settings</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-
-          @if (Auth::guest())
-                              <li><a href="{{ route('login') }}">Login</a></li>
-                              <li><a href="{{ route('register') }}">Register</a></li>
-                          @else
-                              <li class="dropdown">
-                                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                      {{ Auth::user()->name }} <span class="caret"></span>
-                                  </a>
-
-                                  <ul class="dropdown-menu" role="menu">
-                                      <li>
-                                          <a href="{{ route('logout') }}"
-                                              onclick="event.preventDefault();
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
                                                        document.getElementById('logout-form').submit();">
-                                              Logout
-                                          </a>
+                                        Logout
+                                    </a>
 
-                                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                              {{ csrf_field() }}
-                                          </form>
-                                      </li>
-                                  </ul>
-                              </li>
-                          @endif
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
 
-      </ul>
-    </div>
-  </div>
-</nav>
-
-        <div class="container" >
-
-            @yield('content')
+                </ul>
+            </div>
         </div>
-    </div>
+    </nav>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <div class="container" >
+        @yield('content')
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('script')
 </body>
 </html>
