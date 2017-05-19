3c79e1f17e9b8ad365e905599deceb91898dfd64
NzOishie <nazzarreen05@gmail.com>
2017-05-20 04:28:59 +0600

    Survey List Blade

---
+++ resources/views/survey_list.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        @foreach($sueveys as $survey)
+        <div class="alert alert-info">
+            <a href="../summary/{{$survey->id}}">
+                <h3 >{{$survey->title}}<span class="label label-primary" style="float:right">{{$survey->points_per_response}}</span></h3>
+                <h5>{{<?php echo substr($survey->description,30);?>}}</h5>
+
+
+            </a>
+
+        </div>
+        @endforeach
+
+    </div>
+
+</div>
+
+@endsection
