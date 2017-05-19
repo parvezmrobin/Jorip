a985fc11c502e39e9ae25a52aafb4d4efde40028
NzOishie <nazzarreen05@gmail.com>
2017-05-19 12:17:11 +0600

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,64 +12,90 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+    <style >
+
+ .list-inline li {
+           margin: 0px;
+
+           display: table-cell;
+           text-align: center;
+           width: 20%;
+           font-size: xx-large;
+       }
+
+
+ </style>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+        <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header navbar-left pull-left">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+    </div>
+    <div class="navbar-header navbar-right pull-right">
+        <ul class="nav pull-left list-inline ">
+                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-envelope"></span></a></li>
+                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-stats"></span></a></li>
+                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-folder-open"></span></a></li>
+                <li class="navbar-text pull-left"><a href="#"><span class="glyphicon glyphicon-file"></span></a></li>
+                <li class="navbar-text pull-left">
+                    <a href="#" data-toggle="collapse" data-target="#clps">
+                        <span class="glyphicon glyphicon-th-list"></span>
                     </a>
-                </div>
+                </li>
+            </ul>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    </div>
+    <div class="visible-xs-block clearfix"></div>
+    <div class="collapse" id="clps">
+      <ul class="nav navbar-nav navbar-left">
+
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Payment System</a></li>
+          <li><a href="#">Settings</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+
+          @if (Auth::guest())
+                              <li><a href="{{ route('login') }}">Login</a></li>
+                              <li><a href="{{ route('register') }}">Register</a></li>
+                          @else
+                              <li class="dropdown">
+                                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                      {{ Auth::user()->name }} <span class="caret"></span>
+                                  </a>
+
+                                  <ul class="dropdown-menu" role="menu">
+                                      <li>
+                                          <a href="{{ route('logout') }}"
+                                              onclick="event.preventDefault();
+                                                       document.getElementById('logout-form').submit();">
+                                              Logout
+                                          </a>
+
+                                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                              {{ csrf_field() }}
+                                          </form>
+                                      </li>
+                                  </ul>
+                              </li>
+                          @endif
+
+      </ul>
+    </div>
+  </div>
+</nav>
 
         @yield('content')
     </div>
