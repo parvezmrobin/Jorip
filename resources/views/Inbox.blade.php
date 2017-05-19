6c3bdb54e1be1e18f721d3e0f30feed06f776e89
NzOishie <nazzarreen05@gmail.com>
2017-05-19 18:45:32 +0600

    Inbox 2 blade

--- resources/views/Inbox.blade.php
+++ resources/views/Inbox.blade.php
@@ -6,7 +6,7 @@
             @foreach($messages as $var)
                 <div  class="alert alert-info">
                 <a href="#">
-                    <h3 >{{$var->name}}</h3>
+                    <h3 >{{$var->sender->name}}</h3>
                     <h5>{{<?php echo substr($var->body,30);?>}}</h5>
 
                 </a>
